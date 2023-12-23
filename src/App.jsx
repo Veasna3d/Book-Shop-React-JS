ac8f58d938ed9a6c6627f09ac3360403f9d72e7f
Veasna Pen <Veasna3d@gmail.com>
2023-12-23 22:24:38 +0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
-  return <div className="text-3xl font-bold underline">App</div>;
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Logo from "../../assets/website/logo.png";
+
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Seller",
+    link: "/best-seller",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="container py-3 sm:py-0">
+        <div className="flex justify-between">
+          <div>
+            <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+              <img src={Logo} className="w-10" />
+              Books
+            </a>
+          </div>
+          <div>
+            <ul className="items-center gap-4 hidden sm:flex">
+              {Menu.map((menu) => (
+                <li>
+                  <a
+                    href={menu.link}
+                    className="inline-block py-4 px-4 hover:text-primary duration-200"
+                  >
+                    {menu.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
