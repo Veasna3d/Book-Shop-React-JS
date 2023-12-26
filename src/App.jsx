b7020b57e679c40d1c0676716a40e58650a5e8b8
Veasna Pen <Veasna3d@gmail.com>
2023-12-26 21:13:00 +0700

    Feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import BestBooks from "./components/BestBooks/BestBooks";
 
 const App = () => {
   return (
     <>
-      <div>
+      <div className="bg-white dakr:bg-gray-900 dark:text-white duration-200">
         <Navbar />
         <Hero />
+        <BestBooks />
       </div>
     </>
   );
